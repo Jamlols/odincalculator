9127904db079617e8b392e0a57edd3705f1a65af
Kirk <76870582+Jamlols@users.noreply.github.com>
2023-11-02 21:12:00 +0000

    Finish calculator logic

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 let firstNumber;
-let operator;
+let operator = ['+', '-', '/', '*'];
 let secondNumber;
 
 function add(num1, num2) {
@@ -18,5 +18,24 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(multiply(1, 2))
+function operate(operator, num1, num2) {
+    if (operator === '/' && num2 === 0) {
+        return 'Cannot divide by 0!';
+    }
+
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Enter valid operator."
+    }
+}
+
+console.log(operate('/', 5, 0));
 
